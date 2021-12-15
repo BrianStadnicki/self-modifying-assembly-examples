99bdb9be44578e91dcfa2c994e798741c148e83e
Brian Stadnicki <stadnickibrian@gmail.com>
2021-12-15 06:17:39 +0000

    fix execute script after refactor to bin

--- bin/execute.sh
+++ bin/execute.sh
@@ -3,6 +3,6 @@
 nasm -f elf "$1.asm"
 ld -lc --dynamic-linker /lib/i386-linux-gnu/ld-linux.so.2 -m elf_i386 -o "$1.tmp" "$1.o"
 objcopy --writable-text --set-section-flags .text=CONTENTS,ALLOC,LOAD,CODE "$1.tmp" "$1.elf"
-../writable-text "$1.elf"
+../bin/writable-text "$1.elf"
 objdump -M intel -d "$1.elf" > "$1".disasm
 "./$1.elf"
